ddbff2687912184f62d7835b662e73880f1e8423
grantmacken <grantmacken@gmail.com>
2013-05-22 15:36:31 +1200

    adjust a few things

--- config/production.sh
+++ config/production.sh
@@ -11,14 +11,17 @@ if [ ! -d /usr/local/nginx/proxy ] ; then
     mkdir /usr/local/nginx/proxy
 fi
 #common
-cp common/gzip.conf /usr/local/nginx/conf/gzip.conf
-cp common/server-common.conf /usr/local/nginx/conf/server-common.conf
-cp common/proxy-common.conf /usr/local/nginx/conf/proxy-common.conf
+cp -i -v common/* /usr/local/nginx/conf
+#cp common/server-common.conf /usr/local/nginx/conf/server-common.conf
+#cp common/proxy-common.conf /usr/local/nginx/conf/proxy-common.conf
 #production
-cp prod/nginx-prod.conf /usr/local/nginx/conf/nginx.conf
-cp prod/proxy-cache-prior.conf /usr/local/nginx/conf/proxy-cache-prior.conf
-cp prod/proxy-cache.conf /usr/local/nginx/conf/proxy-cache.conf
-cp prod/server-production-locations.conf /usr/local/nginx/conf/server-production-locations.conf
+cp -i -v prod/* /usr/local/nginx/conf
+mv -i -v /usr/local/nginx/conf/nginx-prod.conf /usr/local/nginx/conf/nginx.conf
+
+#cp prod/nginx-prod.conf /usr/local/nginx/conf/nginx.conf
+#cp prod/proxy-cache-prior.conf /usr/local/nginx/conf/proxy-cache-prior.conf
+#cp prod/proxy-cache.conf /usr/local/nginx/conf/proxy-cache.conf
+#cp prod/server-production-locations.conf /usr/local/nginx/conf/server-production-locations.conf
 
 # remove proxy cache
 cd /usr/local/nginx/cache
