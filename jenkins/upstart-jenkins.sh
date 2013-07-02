7a4dda73f3b0a55317c18c6ed0fef91006cdbfcc
grantmacken <grantmacken@gmail.com>
2013-07-02 20:55:42 +1200

    jenkins again

--- jenkins/upstart-jenkins.sh
+++ jenkins/upstart-jenkins.sh
@@ -1,20 +1,30 @@
-#upstart-jenkins-.sh
+#upstart-jenkins.sh
 #===========
 #Grant MacKenzie <grantmacken@gmail.com>
 #v0.1, Feb 2012
 #
 # As Sudo
+# cd jenkins
 #init-checkconf jenkins.conf
-#rm /etc/init/eXist.conf
+#rm /etc/init/jenkins.conf
+# chmod +x upstart-jenkins.sh
+# ./upstart-jenkins.sh
 
-if [ ! -e /etc/init/jenkins.conf ]; then
-   stop jenkins
-fi
+#if [ jenkins -e /etc/init/jenkins.conf ]; then
+#   stop jenkins
+#fi
 
-sleep 10
-cp jenkins.conf /etc/init/jenkins.conf
-sleep 10
-initctl list | grep jenkins
-#initctl show-config jenkins
-sleep 10
-start jenkins
+
+
+IP=$( nslookup 'markup.co.nz'  | tail -2 | awk -F ": " '{print $2}' )
+
+cat jenkins.conf | head -n -1 > jenkins2.conf
+echo --httpListenAddress=$IP >> jenkins2.conf
+#sleep 10
+#
+#cp jenkins.conf /etc/init/jenkins.conf
+#sleep 10
+#initctl list | grep jenkins
+##initctl show-config jenkins
+#sleep 10
+#start jenkins
