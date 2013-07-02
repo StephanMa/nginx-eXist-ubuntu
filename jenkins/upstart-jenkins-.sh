e4587579178ebf11ed7f43d134bc29e7b1fccb21
grantmacken <grantmacken@gmail.com>
2013-07-02 12:46:25 +1200

    nginx jenkins

--- jenkins/upstart-jenkins-.sh
+++ jenkins/upstart-jenkins-.sh
@@ -1,4 +1,4 @@
-#nginx-install
+#upstart-jenkins-.sh
 #===========
 #Grant MacKenzie <grantmacken@gmail.com>
 #v0.1, Feb 2012
@@ -6,7 +6,11 @@
 # As Sudo
 #init-checkconf jenkins.conf
 #rm /etc/init/eXist.conf
-stop jenkins
+
+if [ ! -e /etc/init/jenkins.conf ]; then
+   stop jenkins
+fi
+
 sleep 10
 cp jenkins.conf /etc/init/jenkins.conf
 sleep 10
